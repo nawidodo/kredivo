8846bab0a653557d0f2b7a61a0b1e1bc24aad3da
Nugroho Arief Widodo <nawidodo@gmail.com>
2023-05-27 22:04:21 +0700

    Initial Commit

---
+++ Kredivo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kredivo
+//
+//  Created by Nugroho Arief Widodo on 27/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
